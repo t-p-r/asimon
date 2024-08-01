8dea020d2ad78270964c5ef0378f31e29c163ff3
t-p-r <quanthangthien1000@gmail.com>
2024-08-01 17:43:36 +0700

    tidy up the part around Path.walk()

--- src/lib/models/problem.py
+++ src/lib/models/problem.py
@@ -9,10 +9,10 @@ from lib.utils import text_colors, send_message
 class Problem:
     def __init__(self, problem_dir: Path):
         self.problem_dir = problem_dir
-        self.testdir = problem_dir / "tests"
-        self.testgen_dir = problem_dir / "testgens"
+        self.testdir = problem_dir / "test"
+        self.testgen_dir = problem_dir / "testgen"
 
-        self.solution_dir = problem_dir / "solutions"
+        self.solution_dir = problem_dir / "solution"
         self.solution_other_dir = self.solution_dir / "other_solutions"
 
     def exists(self) -> bool:
@@ -22,15 +22,14 @@ class Problem:
         if self.problem_dir.exists():
             raise Exception("Problem already existed.")
         Path.mkdir(self.problem_dir, parents=True)
-        for p in (self.testdir, self.testgen_dir, self.solution_dir):
+        for p in [self.testdir, self.testgen_dir, self.solution_dir]:
             Path.mkdir(p)
-        for p in (self.solution_mc_dir, self.solution_other_dir):
-            Path.mkdir(p)
+        Path.mkdir(self.solution_other_dir)
 
     def main_correct_solution(self) -> Path:
-        files = []
-        for _, __, filename in self.solution_dir.walk():
-            files += filename
+        _, __, files = self.solution_dir.walk()[0]
+        assert self.solution_dir == _
+        # assert "other_solutions" == __
 
         if len(files) == 0:
             raise Exception(
@@ -40,18 +39,19 @@ class Problem:
             if len(files) > 1:
                 send_message(
                     "More than one main correct solution found, automatically chose the first one.\n \
-                    This is not the behaviour ASIMON expected, and usually results from manual tampering of the problem directory.",
+                    This is not the behaviour ASIMON expected, and should result from manual tampering of the problem directory.",
                     text_colors.YELLOW,
                 )
             return self.solution_dir / files[0]
 
     def other_solutions(self) -> Path:
-        files = []
-        for _, __, filename in self.solution_other_dir.walk():
-            files += self.solution_other_dir / filename
+        _, __, files = self.solution_other_dir.walk()[0]
+        for file in files:
+            file = self.solution_other_dir / file
         if len(files) == 0:
-            raise Warning("No solution other than the main correct one found.")
+            raise Warning("No solution other than the main correct one is found.")
         return files
 
     def test_script(self) -> list:
+        # JSON shit goes here.
         return []
