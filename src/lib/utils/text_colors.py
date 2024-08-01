bd34212a44b97f7696f85145c163c6372f41199d
t-p-r <quanthangthien1000@gmail.com>
2024-08-01 17:41:25 +0700

    move text colors into a class

--- src/lib/utils/text_colors.py
+++ src/lib/utils/text_colors.py
@@ -5,18 +5,19 @@ Some attributes can be combined (e.g. `PURPLE` + `BOLD` + `UNDERLINE`).
 More at: https://www.embedded.pub/linux/misc/escape-codes.html.
 """
 
-# colors
-BLUE = "\033[94m"
-CYAN = "\033[96m"
-GREEN = "\033[92m"
-PURPLE = "\033[95m"
-YELLOW = "\033[93m"
-RED = "\033[91m"
+class text_colors:
+    # colors
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    PURPLE = "\033[95m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
 
-# other attributes
-HEADER = "\033[95m"
-BOLD = "\033[1m"
-UNDERLINE = "\033[4m"
+    # other attributes
+    HEADER = "\033[95m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
-# remove any attributes above
-END_COLOR = "\033[0m"
+    # remove any attributes above
+    END_COLOR = "\033[0m"
