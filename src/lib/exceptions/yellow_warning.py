067f6e93cd44ca896ab58156aa668d4c5b68eab1
t-p-r <quanthangthien1000@gmail.com>
2024-08-07 20:54:42 +0700

    raise issue

--- src/lib/exceptions/yellow_warning.py
+++ src/lib/exceptions/yellow_warning.py
@@ -1,5 +1,7 @@
 from lib.utils.formatting import wrap_message, text_colors
 
+# TODO: fix issue where the program terminates after raising this Warning
+
 
 class YellowWarning(Warning):
     def __init__(self, message: str) -> None:
