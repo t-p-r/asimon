e92ae483b31f2bf622f4fede067b855f2505262f
t-p-r <quanthangthien1000@gmail.com>
2024-08-09 00:25:23 +0700

    update __init__.py

--- src/lib/models/workers/__init__.py
+++ src/lib/models/workers/__init__.py
@@ -1,2 +1,4 @@
-from .test_executor import *
-from .test_generator import *
+from .test_executor import ContestantExecutionResult, WorkerResult, TestExecutor
+from .test_generator import TestGenerator
+
+__all__ = ["ContestantExecutionResult", "WorkerResult", "TestExecutor", "TestGenerator"]
