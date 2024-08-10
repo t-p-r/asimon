8a379925c56393c68349fbe9b3f08240e822062d
t-p-r <quanthangthien1000@gmail.com>
2024-08-10 22:16:09 +0700

    add testlib checker

---
+++ example/workspace/checker.cpp
@@ -0,0 +1,27 @@
+/**
+ * Custom checker using testlib.
+ *
+ * @file example/workspace/checker.cpp
+ *
+ */
+
+#include "lib/testlib.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    registerTestlibCmd(argc, argv);
+    int a = inf.readInt(), b = inf.readInt();
+
+    int c = ans.readInt();  // correct sum
+    if (c != a + b) {
+        quitf(_fail, "Answer is not correct: expected %d, found %d", a + b, c);
+    }
+
+    int d = ouf.readInt();  // possibly incorrect sum
+    if (d != c) {
+        quitf(_wa, "Expected %d, found %d", c, d);
+    }
+
+    quitf(_ok, "ok");
+}
