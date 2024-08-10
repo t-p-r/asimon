b6e7b2548e10c7c8b5b588382d4ae678d16b2d19
t-p-r <quanthangthien1000@gmail.com>
2024-08-10 22:16:06 +0700

    now has a 1% chance to be incorrect

--- example/workspace/contestant.cpp
+++ example/workspace/contestant.cpp
@@ -7,6 +7,8 @@
  * judge). This example takes two numbers as input using the
  * @c cpdsa::buffer_scan function, then prints out their sum using the standard
  * C++ @c cout<< function.
+ *
+ * The answer has a small chance to be incorrect. Can you spot it?
  */
 
 #include <iostream>
@@ -18,5 +20,6 @@ int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int a, b;
     cpdsa::buffer_scan(a, b);
+    if (b % 100 == 0) b--;
     cout << a + b;
 }
